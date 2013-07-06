8080705fa8f604ba8942a55da130866e7e0f31e9
Florian Klampfer <f.klampfer@gmail.com>
2013-07-06 15:21:15 +0200

    add default color

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-var debug = false;
+var debug = false
 if (debug) {
-  var profile = {gravatar_id: "a5b4032eaad882492772f4d6d34a9122"};
+  var profile = {gravatar_id: "a5b4032eaad882492772f4d6d34a9122"}
 } else {
-  var profile = {};
+  var profile = {}
 }
-var languages = {};
-var totalBytes = 0;
-var chart;
+var languages = {}
+var totalBytes = 0
+var chart
 
 $(function(){
 	
-	var self=this;
+	var self=this
 
-  $('#embed').click(function() { $(this).select(); });
+  $('#embed').click(function() { $(this).select() })
   $('#render').click(function() { 
     check = function($s) {
-      var s = $s.val();
+      var s = $s.val()
       return (s >= parseInt($s.attr('min')) && s <= parseInt($s.attr('max')))
     }
     if (check($('#size')) && check($('#radius'))) {
-      updateLanguageGraph();
+      updateLanguageGraph()
     }
-  });
+  })
 	
 	this.showresults=function(username) {
-		$(".result").show();
-    $("#start").hide();
+		$(".result").show()
+    $("#start").hide()
 
-    //_gaq.push(['_trackPageView', '/github-language-graph/'+username]);
+    //_gaq.push(['_trackPageView', '/github-language-graph/'+username])
 
-    var repos = [];
-    var completed = 0;
+    var repos = []
+    var completed = 0
 
-    $("#result-username").text(username);
-    $("#username").val("");
+    $("#result-username").text(username)
+    $("#username").val("")
 
     if (!debug) {
       $.getJSON("https://api.github.com/users/"+username+"?callback=?", function(data) {
@@ -41,48 +41,48 @@ $(function(){
 
         $.getJSON("https://api.github.com/users/"+username+"/repos?callback=?", function(data) {
           $(data.data).each(function(i,d) {
-            repos.push(d.url+"/languages");
-            completed++;
-          });
+            repos.push(d.url+"/languages")
+            completed++
+          })
 
 
-          $("#result-repo-count").text(repos.length + " Repositories");
+          $("#result-repo-count").text(repos.length + " Repositories")
           $(repos).each(function(i,r) {
             $.getJSON(r+"?callback=?", function(data) {
               for(lang in data.data) {
-                var lines = data.data[lang];
+                var lines = data.data[lang]
                 if(!languages[lang]) {
-                  languages[lang] = lines;
+                  languages[lang] = lines
                 } else {
-                  languages[lang] += lines;
+                  languages[lang] += lines
                 }
               }
 
               // draw graph when finished
-              if (!--completed) updateLanguageGraph();
+              if (!--completed) updateLanguageGraph()
 
-            });
-          });
-        });
-      });
+            })
+          })
+        })
+      })
     } else {
-      updateLanguageGraph();
+      updateLanguageGraph()
     }
   }
 	
 	$("#btn-go").click(function(){
-		var username = $("#username").val();
+		var username = $("#username").val()
 		
-		window.location.hash='#'+username;
+		window.location.hash='#'+username
 		
-		self.showresults(username);
+		self.showresults(username)
 
-		return false;
-	});
+		return false
+	})
 
 	if (window.location.hash)
-		self.showresults(window.location.hash.substr(1));
-});
+		self.showresults(window.location.hash.substr(1))
+})
 
 function updateLanguageGraph() {
   if (debug) {
@@ -92,42 +92,43 @@ function updateLanguageGraph() {
       "C#": 288116,
       "Shell": 253,
       "JavaScript": 1069530,
-      "CoffeeScript": 66623
-    };
+      "CoffeeScript": 66623,
+      "XSLT": 66623,
+    }
   }
   
-  var data = [];
+  var data = []
 
-  totalBytes = 0;
+  totalBytes = 0
   for(lang in languages) {
-    data.push({name: lang, y: languages[lang]});
-    totalBytes += languages[lang];
+    data.push({name: lang, y: languages[lang]})
+    totalBytes += languages[lang]
 
   }
 
   data.sort(function(a, b) {
-    return b.y - a.y;
-  });
+    return b.y - a.y
+  })
 
-  $("#language-table tbody").html('');
+  $("#language-table tbody").html('')
   for(var i=0; i<data.length; i++) {
     $("#language-table tbody").append(
       $('<tr>')
         .append($('<td>').text(data[i].name)
-          .prepend($('<span>').html("&#9724 ")).css("color", colors[data[i].name]))
+          .prepend($('<span>').html("&#9724 ")).css("color", colors(data[i].name)))
         .append($('<td>').text(data[i].y))
-    );
+    )
   }
 
-  //chart.series[0].setData(data);  
-  var s = $('#size').val();
+  //chart.series[0].setData(data)
+  var s = $('#size').val()
   var w = s,                        //width
       h = s,                            //height
-      r = s/2;                            //radius
+      r = s/2                            //radius
       avatar = $('#radius').val()/100
 
-  $("#chart").html('');
-  $("#chart").css({position: "relative", width: w, height: h});
+  $("#chart").html('')
+  $("#chart").css({position: "relative", width: w, height: h})
 
   if (avatar != 0) {
     $("#chart").append(
@@ -145,7 +146,7 @@ function updateLanguageGraph() {
             .attr("src", "https://secure.gravatar.com/avatar/"+profile.gravatar_id+"?s="+s)
             .css({ borderRadius: "100%" })
         )
-    );
+    )
   }
    
   if (avatar != 1) {
@@ -157,30 +158,30 @@ function updateLanguageGraph() {
         .append("svg:g")                //make a group to hold our pie chart
             .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
 
-    $("svg").css({position: "relative", zIndex: 2});
+    $("svg").css({position: "relative", zIndex: 2})
 
     var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
         .innerRadius(Math.round(r * avatar))
-        .outerRadius(r);
+        .outerRadius(r)
 
     var pie = d3.layout.pie()           //this will create arc data for us given a list of values
-        .value(function(d) { return d.y; });    //we must tell it out to access the value of each element in our data array
+        .value(function(d) { return d.y })    //we must tell it out to access the value of each element in our data array
 
     var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
         .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
         .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
             .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
                 .attr("class", "slice")     //allow us to style things in the slices (like text)
-                .attr("data-lang-name", function(d) { return d.data.name; })
-                .attr("data-lang-percent", function(d) { return Math.round(d.data.y/totalBytes*100) + "%"; })
-                .attr("data-lang-color", function(d) { return colors[d.data.name]; });
+                .attr("data-lang-name", function(d) { return d.data.name })
+                .attr("data-lang-percent", function(d) { return Math.round(d.data.y/totalBytes*100) + "%" })
+                .attr("data-lang-color", function(d) { return colors(d.data.name) })
 
         arcs.append("svg:path")
-                .attr("fill", function(d, i) { return colors[d.data.name]; } ) //set the color for each slice to be chosen from the color function defined above
-                .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+                .attr("fill", function(d, i) { return colors(d.data.name) } ) //set the color for each slice to be chosen from the color function defined above
+                .attr("d", arc)                                     //this creates the actual SVG path using the associated data (pie) with the arc drawing function
   }
 
-  var fontSize = Math.round(s*avatar/6);
+  var fontSize = Math.round(s*avatar/6)
 
   //for(var i=0; i<data.length; i++) {
     $('#chart-inner').append(
@@ -192,7 +193,7 @@ function updateLanguageGraph() {
         position: "absolute",
         top: 0,
         left: 0,
-        //backgroundColor: colors[data[i].name],
+        //backgroundColor: colors(data[i].name),
         color: "#fff",
         borderRadius: "100%",
         paddingTop: Math.round((s*avatar/2) - fontSize) + "px",
@@ -201,7 +202,7 @@ function updateLanguageGraph() {
         boxSizing: "border-box",
         opacity: 0
       })
-    );
+    )
   //}
 
   var script = ""+
@@ -228,8 +229,12 @@ var replaceSpecialChars = function(text) {
 }
 */
 
+function colors(lang) {
+  return githubLanguageColors[lang] || "#ccc"
+}
+
 // https://github.com/doda/github-language-colors
-colors = {
+githubLanguageColors = {
     "Arduino": "#bd79d1",
     "Java": "#b07219",
     "VHDL": "#543978",
@@ -306,4 +311,4 @@ colors = {
     "Smalltalk": "#596706",
     "Matlab": "#bb92ac",
     "C#": "#bb92af"
-};
+}
